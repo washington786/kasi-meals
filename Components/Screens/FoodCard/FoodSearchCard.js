193a9c35fffc7aed90f696a0d1ae7b8c7a120c7f
washington786 <75230446+washington786@users.noreply.github.com>
2022-03-22 19:37:32 +0200

    commit

---
+++ Components/Screens/FoodCard/FoodSearchCard.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FoodSearchCard = ({
+    onPressFoodCard,
+    restaurantName,
+    deliveryAvailable,
+    discountAvailable,
+    discountPercent,
+    numberOfReviews,
+    averageReviews,
+    businessAddress,
+    farAway,
+    images,
+    productData
+}) => {
+  return (
+    <View>
+      
+    </View>
+  )
+}
+
+export default FoodSearchCard
+
+const styles = StyleSheet.create({
+    
+})
